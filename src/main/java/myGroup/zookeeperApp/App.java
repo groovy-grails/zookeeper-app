d6fc036a8af189d94b301275c664ba6e07cf7ecd
bros <groovy@aliyun.com>
2018-07-16 12:45:50 +0800

    first

---
+++ src/main/java/myGroup/zookeeperApp/App.java
@@ -0,0 +1,13 @@
+package myGroup.zookeeperApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
